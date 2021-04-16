3f4801656ad9396586a1986242de198f5f154b57
Harshit Pant <pantharshit00@gmail.com>
2021-04-16 16:49:27 +0530

    upgrade to 2.21.2

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,32 +1,34 @@
-const { objectType, queryField, list } = require('nexus');
+const { objectType, queryField, list } = require("nexus");
+const { Prisma } = require("@prisma/client");
 
 exports.Child = objectType({
-	name: 'Child',
-	definition(t) {
-		t.nonNull.string('id');
-	},
+  name: "Child",
+  definition(t) {
+    t.nonNull.string("id");
+  },
 });
 
 exports.Parent = objectType({
-	name: 'Parent',
-	definition(t) {
-		t.nonNull.string('id');
-		t.nonNull.field('child', {
-			type: 'Child',
-			resolve: async (parent, args, ctx) => {
-				return ctx.prisma.child.findUnique({
-					where: {
-						id: parent.childId,
-					},
-				});
-			},
-		});
-	},
+  name: "Parent",
+  definition(t) {
+    t.nonNull.string("id");
+    t.nonNull.field("child", {
+      type: "Child",
+      resolve: async (parent, args, ctx) => {
+        console.log(parent.childId);
+        return ctx.prisma.child.findFirst({
+          where: {
+            id: parent.childId.toNumber(),
+          },
+        });
+      },
+    });
+  },
 });
 
-exports.ParentQuery = queryField('parents', {
-	type: list('Parent'),
-	resolve: (parent, args, ctx) => {
-		return ctx.prisma.parent.findMany();
-	},
+exports.ParentQuery = queryField("parents", {
+  type: list("Parent"),
+  resolve: (parent, args, ctx) => {
+    return ctx.prisma.parent.findMany();
+  },
 });
